9772ebea986e19d95ad4c5c9b26f101ba9cb42db
José Robert <robert.suporteweb@gmail.com>
2021-12-10 16:31:09 -0300

    Modifica name class

--- ms-hr-eureka-server/src/main/java/br/com/microservices/hreurekaserver/HrEurekaServerApplication.java
+++ ms-hr-eureka-server/src/main/java/br/com/microservices/hreurekaserver/HrEurekaServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MsHrEurekaServerApplication {
+public class HrEurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MsHrEurekaServerApplication.class, args);
+		SpringApplication.run(HrEurekaServerApplication.class, args);
 	}
 
 }
