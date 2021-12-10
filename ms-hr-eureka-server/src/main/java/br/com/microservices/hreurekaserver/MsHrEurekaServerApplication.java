bd79ac9a8d9dc9ab208365cd9a2228539088ba2b
José Robert <robert.suporteweb@gmail.com>
2021-12-10 16:28:47 -0300

    Project hr-eureka-server created

---
+++ ms-hr-eureka-server/src/main/java/br/com/microservices/hreurekaserver/MsHrEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.microservices.hreurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsHrEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsHrEurekaServerApplication.class, args);
+	}
+
+}
