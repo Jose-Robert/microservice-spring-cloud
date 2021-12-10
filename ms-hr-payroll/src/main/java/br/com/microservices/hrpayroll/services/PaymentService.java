5b8815bc07620c83505d2176a68e077fa957f2e3
José Robert <robert.suporteweb@gmail.com>
2021-12-10 12:50:26 -0300

    feign cleints

--- ms-hr-payroll/src/main/java/br/com/microservices/hrpayroll/services/PaymentService.java
+++ ms-hr-payroll/src/main/java/br/com/microservices/hrpayroll/services/PaymentService.java
@@ -2,28 +2,18 @@ package br.com.microservices.hrpayroll.services;
 
 import br.com.microservices.hrpayroll.entities.Payment;
 import br.com.microservices.hrpayroll.entities.Worker;
+import br.com.microservices.hrpayroll.feignclients.WorkerFeignClients;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClients workerFeignClients;
 
     public Payment getPayment(long workerId, int days) {
-
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", String.valueOf(workerId));
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        Worker worker = workerFeignClients.findById(workerId).getBody();
         return Payment.builder()
                 .name(worker.getName())
                 .dailyIncome(worker.getDailyIncome())
