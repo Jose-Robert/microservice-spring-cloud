cee28da726439035f82747d0f1e4004b56f84e5b
José Robert <robert.suporteweb@gmail.com>
2021-12-10 18:13:18 -0300

    Project hr-api-gateway-zull

---
+++ ms-hr-api-gateway-zull/src/main/java/br/com/microservices/hrapigatewayzull/MsHrApiGatewayZullApplication.java
@@ -0,0 +1,13 @@
+package br.com.microservices.hrapigatewayzull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsHrApiGatewayZullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsHrApiGatewayZullApplication.class, args);
+	}
+
+}
---
+++ ms-hr-api-gateway-zull/src/test/java/br/com/microservices/hrapigatewayzull/MsHrApiGatewayZullApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.microservices.hrapigatewayzull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsHrApiGatewayZullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
